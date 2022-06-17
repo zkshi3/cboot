2af0367b67301163cec7439426f27828f4f59ccc
zkshi <984423585.@qq.com>
2022-06-17 14:40:09 +0800

    second commit

--- src/main/java/com/lz/ht/CbootApplication.java
+++ src/main/java/com/lz/ht/CbootApplication.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class  CbootApplication {
 
     public static void main(String[] args) {
+        System.out.println("---------------start-----------------");
         SpringApplication.run( CbootApplication.class, args);
     }
 
